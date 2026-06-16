199c313c0b0e519de5a6887bd3b58de5990cc275
Manish Kumar <mandecent.gupta@gmail.com>
2026-06-16 05:07:45 +0000

    Update view.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,13 @@ export default defineConfig({
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'ReactJSXRuntime',
+          'react/jsx-dev-runtime': 'ReactJSXDevRuntime',
         },
       },
     },
